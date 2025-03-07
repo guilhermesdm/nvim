b912e214ca34584771b66bb6aa3f9f45c1b2295d
Guilherme <guilhermesanm2003@gmail.com>
2025-03-07 09:06:11 -0300

    fix: changing lsp/cmp

--- lua/plugins/modules/mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ 'williamboman/mason.nvim', 
-    config = function() require('mason').setup {} end,
-    opts = { ensure_installed = {"prettier"} }
-  },
-	{ 'williamboman/mason-lspconfig.nvim'},
-}
