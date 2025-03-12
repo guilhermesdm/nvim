2680f631c9ab3e6ee3d78a5f3b726d5e097503d1
Guilherme <guilhermesanm2003@gmail.com>
2025-03-11 23:07:57 -0300

    feat: new lualine config

--- lua/plugins/modules/lualine.lua
+++ lua/plugins/modules/lualine.lua
@@ -1,7 +1,22 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	requires = { "nvim-tree/nvim-web-devicons" },
-  options = {
-    theme = 'bubbles_theme',
-  }
+  config = function ()
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = 'dracula',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 100,
+        }
+      },
+    })
+  end
 }
